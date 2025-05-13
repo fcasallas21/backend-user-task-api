171cc4716ff07109ddc113562843ebc7454f1a19
fcasallas21 <fcasallas21@gmail.com>
2025-05-13 15:08:25 -0500

    Part2: Construcción de API RESTful

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class TasksController < ApplicationController
+      before_action :set_task, only: [:show, :update, :destroy]
+      def index
+        tasks = Task.all
+        render json: tasks
+      end
+
+      def show
+        render json: @task
+      end
+
+      def create
+        task = Task.new(task_params)
+        if task.save
+          render json: task, status: :created
+        else
+          render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @task.update(task_params)
+          render json: @task
+        else
+          render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @task.destroy
+        render json: { message: "REGISTRO ELIMINADO CORRECTAMENTE" }, status: :ok
+      end
+
+      private
+
+      def set_task
+        @task = Task.find(params[:id])
+      end
+
+      def task_params
+        params.require(:task).permit(:title, :description, :status, :due_date, :user_id)
+      end
+    end
+  end
+end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,4 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email, :full_name, :role
+  has_many :tasks
+end
